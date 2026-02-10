5d0cf09880dc85d5b8016c83603c8ae7974cf1f5
JasperRosales <23-19300@g.batstate-u.edu.ph>
2026-02-10 20:00:03 +0800

    init setup

---
+++ Makefile
@@ -0,0 +1,16 @@
+dev:
+	air
+
+build:
+	@go build -o bin/app cmd/app/main.go
+
+push:
+	@git add .
+	@git commit -m "$(m)"
+	@git push origin main
+
+branch:
+	@git checkout -b $(b)
+	@git add .
+	@git commit -m "$(m)"
+	@git push --set-upstream origin $(b)
---
+++ cmd/api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+
+	"github.com/JasperRosales/aircraft-system-be/internal/middleware"
+)
+
+func main() {
+
+	godotenv.Load()
+
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout", "logs/api.log"}
+	logger, err := config.Build()
+	if err != nil {
+		panic("Failed to initialize logger: " + err.Error())
+	}
+	defer logger.Sync()
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(middleware.LoggerMiddleware(logger))
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message":     "Aircraft API is running...",
+			"version":     "1.0.0",
+			"description": "This API provides information about aircrafts, including their specifications, performance, and history.",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+	router.Run(":" + port)
+}
