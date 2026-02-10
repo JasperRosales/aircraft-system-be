e5254b554f9c18a209da30bd786da4e8b6f12ac4
JasperRosales <23-19300@g.batstate-u.edu.ph>
2026-02-10 21:13:38 +0800

    added the database connection, migration,  and docker support

---
+++ database/migrations/20260210125023_create_table.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(100) NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +goose Down
+DROP TABLE users;
